f752b23c839b2b5c9deed7bbcbb526fb672109ba
Leonardo Lopez <lopezmezaleonardo553@gmail.com>
2024-10-21 08:14:40 -0600

    Proyecto

---
+++ Cinepolis/src/Main.java
@@ -0,0 +1,13 @@
+import cine.Cine;
+import menu.Menu;
+import usuarios.cliente.Cliente;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.login();
+
+    }
+}
