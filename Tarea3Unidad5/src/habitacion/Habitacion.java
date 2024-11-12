527bf9edf0d3ec451f59a6de0e4abaf8482cbf2e
Leonardo Lopez <lopezmezaleonardo553@gmail.com>
2024-11-11 20:12:42 -0600

    Tarea terminada

---
+++ Tarea3Unidad5/src/habitacion/Habitacion.java
@@ -0,0 +1,14 @@
+package habitacion;
+
+public class Habitacion {
+    public String tipo;
+    public double precioPorNoche;
+    public boolean disponible;
+
+    public Habitacion(String tipo, double precioPorNoche, boolean disponible) {
+        this.tipo = tipo;
+        this.precioPorNoche = precioPorNoche;
+        this.disponible = disponible;
+
+    }
+}
